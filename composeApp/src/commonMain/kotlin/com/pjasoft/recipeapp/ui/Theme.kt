93f7c0cdcc8ea47d3ea45e07c5d6f64219f15577
casizzy <scr38919@lasallebajio.edu.mx>
2025-11-21 17:54:43 -0600

    finalmente

--- composeApp/src/commonMain/kotlin/com/pjasoft/recipeapp/ui/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/pjasoft/recipeapp/ui/Theme.kt
@@ -2,11 +2,20 @@ package com.pjasoft.recipeapp.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.Font
+import recipeapp512.composeapp.generated.resources.SimpleHandmade
+import recipeapp512.composeapp.generated.resources.Res
+
 
 private val LightColors = lightColorScheme(
     primary = PrimaryColorLight,
@@ -35,12 +44,53 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
+fun SimpleHandmadeFontFamily() : FontFamily{
+    return FontFamily(
+        Font(Res.font.SimpleHandmade)
+    )
+}
+
+@Composable
+fun HeyCommicTypo() = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = SimpleHandmadeFontFamily(),
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SimpleHandmadeFontFamily(),
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = SimpleHandmadeFontFamily(),
+        fontSize = 12.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = SimpleHandmadeFontFamily(),
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = SimpleHandmadeFontFamily(),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    labelLarge = TextStyle(
+        fontFamily = SimpleHandmadeFontFamily(),
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium
+    )
+)
+
+
+@Composable
 fun RecipeTheme(
     content : @Composable () -> Unit
 ){
-    val colors = if(isSystemInDarkTheme()) DarkColors else LightColors
-    MaterialTheme (
+    val colors = if (isSystemInDarkTheme()) DarkColors else LightColors
+
+    MaterialTheme(
         colorScheme = colors,
+        typography = HeyCommicTypo(),
         content = content
-        )
+    )
 }
